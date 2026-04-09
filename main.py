c6330e587262d7d6ff125a249b5e9c36245a0444
Feng Jianming <fengjianming@mails.neu.edu.cn>
2026-04-09 21:02:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 print(f"test")
+print(f"hello,World!")
+
+import os
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+
+
+for path, root ,files in os.walk(../file)
+  print(f"位置{path}")
+
+
+
+
+
